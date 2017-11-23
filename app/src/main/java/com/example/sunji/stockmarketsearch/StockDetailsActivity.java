00e4881cd3438fb19c335c662bdba0021a39ea35
Jiepeng Sun <jiepengs@usc.edu>
2017-11-23 08:45:51 -0800

    4 Basic View Pager and Title

--- app/src/main/java/com/example/sunji/stockmarketsearch/StockDetailsActivity.java
+++ app/src/main/java/com/example/sunji/stockmarketsearch/StockDetailsActivity.java
@@ -18,6 +18,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class StockDetailsActivity extends AppCompatActivity {
 
@@ -25,6 +26,10 @@ public class StockDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock_details);
+
+        // Set Action Bar Title
+        String actionbarTitle = getIntent().getStringExtra("symbol");
+        setTitle(actionbarTitle);
 
         // Create the adapter that will return a fragment for each of the three primary sections of the activity.
         SectionsPagerAdapter mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
@@ -35,6 +40,7 @@ public class StockDetailsActivity extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.viewPagerTabLayout);
         tabLayout.setupWithViewPager(mViewPager);
+
     }
 
     @Override
@@ -88,51 +94,4 @@ public class StockDetailsActivity extends AppCompatActivity {
         }
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView;
-            switch (getArguments().getInt(ARG_SECTION_NUMBER)) {
-                case 1:
-                    rootView = inflater.inflate(R.layout.fragment_current, container, false);
-                    break;
-                case 2:
-                    rootView = inflater.inflate(R.layout.fragment_historial, container, false);
-                    break;
-                case 3:
-                    rootView = inflater.inflate(R.layout.fragment_news, container, false);
-                    break;
-                default:
-                    rootView = inflater.inflate(R.layout.fragment_stock_details, container, false);
-                    TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-                    textView.setText("Hello World");
-            }
-            return rootView;
-        }
-    }
 }
