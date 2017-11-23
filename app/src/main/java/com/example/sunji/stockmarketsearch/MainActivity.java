34466acbe39cd53b699814a5519c09f8a5f34227
Jiepeng Sun <jiepengs@usc.edu>
2017-11-22 22:18:09 -0800

    3 Basic View Pager

--- app/src/main/java/com/example/sunji/stockmarketsearch/MainActivity.java
+++ app/src/main/java/com/example/sunji/stockmarketsearch/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.sunji.stockmarketsearch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +10,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQ_CODE_STOCK_DETAILS_ACTIVITY = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +26,10 @@ public class MainActivity extends AppCompatActivity {
                 EditText text = (EditText) findViewById(R.id.stockQuoteInput);
                 String input = text.getText().toString();
                 if(!input.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "The Inout is: " + input, Toast.LENGTH_LONG).show();
+                    // Toast.makeText(MainActivity.this, "The Inout is: " + input, Toast.LENGTH_LONG).show();
+                    // Active Stock Details Activity
+                    Intent intent = new Intent(MainActivity.this, StockDetailsActivity.class);
+                    startActivityForResult(intent, REQ_CODE_STOCK_DETAILS_ACTIVITY);
                 } else {
                     Toast.makeText(MainActivity.this, "Please Enter a Stock Name or Symbol", Toast.LENGTH_LONG).show();
                 }
@@ -34,12 +40,12 @@ public class MainActivity extends AppCompatActivity {
         // Click Clear Button
         TextView clear = (TextView) findViewById(R.id.clear);
         clear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Toast.makeText(MainActivity.this, "Click the Clear Button", Toast.LENGTH_LONG).show();
-                String newString = "";
-                ((TextView) findViewById(R.id.stockQuoteInput)).setText(newString);
-            }
-        });
+             @Override
+             public void onClick(View view) {
+                 //Toast.makeText(MainActivity.this, "Click the Clear Button", Toast.LENGTH_LONG).show();
+                 String newString = "";
+                 ((TextView) findViewById(R.id.stockQuoteInput)).setText(newString);
+             }
+         });
     }
 }
